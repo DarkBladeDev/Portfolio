d15a4af263a2c8a03211185e467e09de84e820b5
DarkBladeDev <darkymodz555@gmail.com>
2025-04-27 11:37:45 -0300

    refactor: update redirect URL and personal info in index.html
    Updated the redirect URL in load_switch.js to point to the GitHub repository for VIRTHA-Events. Also, modified the personal information in index.html to reflect current skills, projects, and age.

--- javascript/load_switch.js
+++ javascript/load_switch.js
@@ -1,4 +1,4 @@
 // Redireccionamos a gofiles.html después de 3 segundos
 setTimeout(function () {
-    window.location.href = '../Portfolio/gofiles.html';
+    window.location.href = 'https://github.com/VIRTHA/VIRTHA-Events';
 }, 4000);
